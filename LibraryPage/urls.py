63890c6b4e22b78ca36dcfcc852920a86b764fb0
nk-on <2nkoz@proton.me>
2025-11-21 21:22:46 +0400

    Feat:implementing feature which let's user to render data about individual book

--- LibraryPage/urls.py
+++ LibraryPage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import allBooks
+from .views import allBooks,book
 urlpatterns = [
-    path('',allBooks)
+    path('',allBooks),
+    path('<str:book>',book)
 ]
--- LibraryPage/views.py
+++ LibraryPage/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
+
+books = {
+    "harry-potter": {"title": "Harry Potter", "author": "J.K. Rowling", "rating": 9},
+    "hobbit": {"title": "The Hobbit", "author": "J.R.R. Tolkien", "rating": 8},
+    "gatsby": {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "rating": 7,
+    },
+}
+
 
 # Create your views here.
 def allBooks(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    booksListing = ''
+    for key in books:
+        booksListing+=f"<li>{key}</li>"
+    return HttpResponse(f"<ul>{booksListing}</ul>")
+def book(request,book):
+    try:
+        return HttpResponse(f"{books[book]}")
+    except:
+        return HttpResponseNotFound("<h1>404 Not found</h1>")
